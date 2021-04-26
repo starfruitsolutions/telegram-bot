1fe919dd22ecfe72eb315c6d03129e8d2eafa2df
Brad-Lee Nichols <bradlee@starfruitsolutions.com>
2021-04-26 15:10:00 -0400

    Adds auth

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+//Amplify
 import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-vue';
 import aws_exports from './aws-exports';
-import {
-  applyPolyfills,
-  defineCustomElements,
-} from '@aws-amplify/ui-components/loader';
 
 Amplify.configure(aws_exports);
-applyPolyfills().then(() => {
-  defineCustomElements(window);
-});
 
 
 Vue.config.productionTip = false
