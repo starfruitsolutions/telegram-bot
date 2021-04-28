329d6c1dd3b5b76421b69f466ebc5efbc24bf5d3
Brad-Lee Nichols <brad-lee@euler.tools>
2021-04-28 15:16:03 -0400

    Adds basic global components

---
+++ front-end/src/components/global/Card.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-card
+    :class="{ 'mb-5': margins}"
+    color="white"
+    class="card elevation-3"
+  >
+    <v-toolbar
+       class="px-6"
+      :class="{'clickable': expandable}"
+      color="deep-purple accent-3"
+      dark
+      height="100"
+      @click="toggle()">
+      <v-toolbar-side-icon v-if="expandable">
+        <v-icon v-if="!expanded">fa-plus-square</v-icon>
+        <v-icon v-if="expanded">fa-minus-square</v-icon>
+      </v-toolbar-side-icon>
+      <v-toolbar-title>
+        <h3>
+          <slot name = "header"/>
+        </h3>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <slot name = "extras"/>
+    </v-toolbar>
+    <p
+      v-if="!expandable || expanded"
+      :class="{ 'pa-10': padding}"
+    >
+      <slot/>
+    </p>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    expandable: {
+      type: Boolean,
+      default: false
+    },
+    padding: {
+      type: Boolean,
+      default: true
+    },
+    margins: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    return {
+      expanded: false
+    }
+  },
+  methods: {
+    toggle () {
+      this.expanded = !this.expanded
+    }
+  }
+}
+</script>
---
+++ front-end/src/components/global/Modal.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-dialog
+    v-model="visible"
+    width="600"
+    @input="cancel"
+  >
+    <v-card>
+      <v-card-title
+        class="px-10 white--text deep-purple accent-3"
+      >
+        {{ title }}
+      </v-card-title>
+
+      <v-card-text class="pa-10">
+        <slot/>
+      </v-card-text>
+
+      <v-card-actions class="px-10 pb-10">
+        <v-spacer/>
+        <v-btn
+          v-if="okVisible"
+          color="deep-purple"
+          dark
+          large
+          @click="ok"
+        >
+          {{ okText }}
+        </v-btn>
+        <v-btn
+          v-if="cancelVisible"
+          large
+          color="white"
+          @click="cancel"
+        >
+          Cancel
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    okText: {
+      type: String,
+      default: 'Ok'
+    },
+    okVisible: {
+      type: Boolean,
+      default: true
+    },
+    cancelVisible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    classes () {
+      return ['modal', 'animated', 'is-active']
+    }
+  },
+  methods: {
+    ok () {
+      this.$emit('ok')
+    },
+    cancel () {
+      this.$emit('cancel')
+    }
+  }
+}
+</script>
