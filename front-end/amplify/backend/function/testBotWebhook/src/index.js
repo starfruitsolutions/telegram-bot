a8032824f8b8d02eb56666fe76b2534ffb368af8
Brad-Lee Nichols <brad-lee@euler.tools>
2021-05-06 03:34:32 -0400

    Adds template formatter support and basic arithmetic formatters

--- front-end/amplify/backend/function/testBotWebhook/src/index.js
+++ front-end/amplify/backend/function/testBotWebhook/src/index.js
@@ -6,28 +6,24 @@
 Amplify Params - DO NOT EDIT */
 const axios = require('axios')
 var Mustache = require('mustache')
+const Wax = require('@jvitela/mustache-wax');
+
+/**********************
+ * environment constants *
+ **********************/
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
+const BOT_ID = process.env.BOT_ID
+
+/**********************
+ * graphql *
+ **********************/
 const {GraphQLGateway, IAMCredentialsStrategy} = require('@crft/appsync-gateway')
 const creds = new IAMCredentialsStrategy();
-
 const gateway = new GraphQLGateway(
   creds,
   process.env.API_BOT_GRAPHQLAPIENDPOINTOUTPUT
 )
 
-const TELEGRAM_URL = 'https://api.telegram.org/bot'
-const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
-const DEBUG_CHAT = process.env.DEBUG_CHAT
-const API_URL = process.env.API_BOT_GRAPHQLAPIENDPOINTOUTPUT
-const API_ID = process.env.API_BOT_GRAPHQLAPIIDOUTPUT
-const API_KEY = process.env.API_KEY
-const BOT_ID = process.env.BOT_ID
-
-/**********************
- * Bot handler *
- **********************/
-const exit = {
-  statusCode: 200
-}
 const getCommand = `
   query getCommand($bot: ID!, $name: String!) {
     getBot(id: $bot) {
@@ -49,9 +45,79 @@ const getCommand = `
   }
  `
 
+ /**********************
+  * templating *
+  **********************/
+Wax(Mustache)
+Mustache.Formatters = {
+  "add": function (one, two) {
+    return Number(one) + Number(two)
+  },
+  "subtract": function (one, two) {
+    return Number(one) - Number(two)
+  },
+  "multiply": function (one, two) {
+    return Number(one) * Number(two)
+  },
+  "divide": function (one, two) {
+    return Number(one) / Number(two)
+  }
+}
+
+function render(template, data){
+  console.log('render template')
+  return Mustache.render(template, data)
+}
+
+/**********************
+ * process *
+ **********************/
+function parseCommand(message) {
+ console.log('parse command')
+ // if it's not properly formed stop
+ if(!message.entities || message.entities[0].type != 'bot_command' || message.entities[0].offset != '0'){
+   console.log('malformed command')
+   return false
+ }
+ var args = message.text.split(/\s+/)
+ args[0] = args[0].split('@')[0] // tosses @user
+
+ return args
+}
+
+async function getConfig(args){
+ console.log('performing query')
+ return gateway.runQuery({
+   operationName: 'getCommand',
+   query: getCommand,
+   variables: {
+     bot: BOT_ID,
+     name: args[0] // first arg is command
+   }
+ })
+}
+
+async function fetchSources(sources, templateData){
+ console.log('fetching sources')
+ var data = {}
+
+ for (i = 0; i < sources.length; i++) {
+   console.log(sources[i])
+
+   var response = await axios({
+     url: render(sources[i].url, templateData), // render allows args in definition
+     method: render(sources[i].method, templateData),
+     data: (sources[i].body ? JSON.parse(render(sources[i].body, templateData)) : null) // can't render if null
+   })
+   console.log(response)
+   data[sources[i].name] = response.data
+ }
+ return data
+}
+
 function sendMessage(chat_id, text) {
   return axios({
-    url: `${TELEGRAM_URL}${TELEGRAM_TOKEN}/sendMessage`,
+    url: `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`,
     method: 'POST',
     data: {
       chat_id,
@@ -60,56 +126,11 @@ function sendMessage(chat_id, text) {
   })
 }
 
-function debug(text) {
-  return sendMessage(DEBUG_CHAT, text)
-}
-
-function parseCommand(message) {
-  console.log('parse command')
-  // if it's not properly formed stop
-  if(!message.entities || message.entities[0].type != 'bot_command' || message.entities[0].offset != '0'){
-    console.log('malformed command')
-    return false
-  }
-  var args = message.text.split(/\s+/)
-  args[0] = args[0].split('@')[0] // tosses @user
-
-  return args
-}
-
-async function getConfig(args){
-  console.log('performing query')
-  return gateway.runQuery({
-    operationName: 'getCommand',
-    query: getCommand,
-    variables: {
-      bot: BOT_ID,
-      name: args[0] // first arg is command
-    }
-  })
-}
-
-function render(template, data){
-  console.log('render template')
-  return Mustache.render(template, data)
-}
-
-async function fetchSources(sources, templateData){
-  console.log('fetching sources')
-  var data = {}
-
-  for (i = 0; i < sources.length; i++) {
-    console.log(sources[i])
-
-    var response = await axios({
-      url: render(sources[i].url, templateData), // render allows args in definition
-      method: render(sources[i].method, templateData),
-      data: (sources[i].body ? JSON.parse(render(sources[i].body, templateData)) : null) // can't render if null
-    })
-    console.log(response)
-    data[sources[i].name] = response.data
-  }
-  return data
+/**********************
+ * Bot handler *
+ **********************/
+const exit = {
+  statusCode: 200
 }
 
 exports.handler = async (event, context) => {
