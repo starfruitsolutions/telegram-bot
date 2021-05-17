bf5ab076ab360700f249c1955cff87947da2becb
Brad-Lee Nichols <brad-lee@euler.tools>
2021-05-17 13:56:42 -0400

    Adds basic announcement endpoint

---
+++ front-end/amplify/backend/function/announcementHook/src/index.js
@@ -0,0 +1,46 @@
+const axios = require('axios')
+
+/**********************
+ * environment constants *
+ **********************/
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
+
+/**********************
+ * process *
+ **********************/
+
+function sendMessage(chat_id, text) {
+  return axios({
+    url: `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`,
+    method: 'POST',
+    data: {
+      chat_id,
+      text
+    }
+  })
+}
+
+/**********************
+ * Bot handler *
+ **********************/
+exports.handler = async (event, context) => {
+  console.log(process.env)
+  try {
+    console.log('parse body')
+    const body = JSON.parse(event.body)
+    console.log(body)
+
+    await sendMessage(body.channel, body.message)
+  }
+  catch(error) {
+    return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify("Message sent")
+  }
+}
