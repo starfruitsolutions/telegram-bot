940f9b328a51e9d5b92598f1cd8829e508e4005a
Brad-Lee Nichols <brad-lee@euler.tools>
2021-05-31 12:00:04 -0400

    Removes unnecessary form validation types

--- src/views/login.vue
+++ src/views/login.vue
@@ -6,47 +6,9 @@
         justify="center"
       >
         <amplify-authenticator/>
-
-        <!--
-        <v-card
-          width="400"
-          class="pa-4"
-        >
-          <v-container>
-            <v-form
-              ref="form"
-              v-model="valid"
-              @submit.prevent="submit"
-            >
-              <v-text-field
-                v-model="username"
-                :rules="[validationRules.required]"
-                label="Username"
-                autocomplete="username"
-                prepend-icon="fa-user"
-              />
-              <v-text-field
-                v-model="password"
-                type='password'
-                :rules="[validationRules.required]"
-                label="Password"
-                autocomplete="current-password"
-                prepend-icon="fa-unlock-alt"
-              />
-              <v-btn
-                :disabled="!valid"
-                type="submit"
-                width="100%"
-                class="mt-3 mb-6"
-              >
-                Submit
-              </v-btn>
-            </v-form>
-          </v-container>
-        </v-card>
-      -->
       </v-row>
     </v-container>
+    
 </template>
 
 <script>
@@ -55,30 +17,10 @@ import { mapMutations } from 'vuex'
 
 export default {
   name: 'Login',
-  /*
-  data () {
-    return {
-      valid: false,
-      username: null,
-      password: null
-    }
-  },
-  */
   methods: {
     ...mapMutations({
       setUser: 'setUser'
     }),
-    /*
-    async submit () {
-      try {
-        const user = await Auth.signIn(this.username, this.password)
-        this.setUser(user)
-        this.$router.push('/')
-      } catch (error) {
-        console.log('error signing in', error)
-      }
-    }
-    */
   },
   created() {
     // authentication state managament
