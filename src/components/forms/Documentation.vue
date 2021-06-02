48623d7b4e1e3e5d2208a31b3dc8920f6ff3ad7a
Brad-Lee Nichols <bradlee@starfruitsolutions.com>
2021-06-02 15:53:35 -0400

    Updates on page documentation

--- src/components/forms/Documentation.vue
+++ src/components/forms/Documentation.vue
@@ -1,5 +1,37 @@
 <template>
-  <div v-pre class="mt-5">
+  <div class="mt-5">
+    <h4> Templating </h4>
+    <br>
+    <p>
+      The templating engine uses EJS allowing for embedded javascript templates. Templates can be used in data source fields and the final message template.
+    </p>
+    <p>
+       You can render a variable value by using:
+       <code>&lt;%= variable %&gt;</code>
+    </p>
+    <p> List of tags: </p>
+    <p>
+      <ul>
+        <li><code>&lt;%</code> 'Scriptlet' tag, for control-flow, no output</li>
+        <li><code>&lt;%_</code> ‘Whitespace Slurping’ Scriptlet tag, strips all whitespace before it</li>
+        <li><code>&lt;%=</code> Outputs the value into the template (HTML escaped)</li>
+        <li><code>&lt;%-</code> Outputs the unescaped value into the template</li>
+        <li><code>&lt;%#</code> Comment tag, no execution, no output</li>
+        <li><code>&lt;%%</code> Outputs a literal '&lt;%'</li>
+        <li><code>%&gt;</code> Plain ending tag</li>
+        <li><code>-%&gt;</code> Trim-mode ('newline slurp') tag, trims following newline</li>
+        <li><code>_%&gt;</code> ‘Whitespace Slurping’ ending tag, removes all whitespace after it</li>
+      </ul>
+    </p>
+    <h4> Available Data </h4>
+    <br>
+    <pre>
+      {
+        args: [], // Command arguments starting with the command itself at index 0: args[index]
+        message: {} // The <a href="https://core.telegram.org/bots/api#update" target="_blank">message</a> from the telegram update: message.[field]...
+        sources: {} // Data sources can be referenced by their assigned name: sources.[name]
+      }
+    </pre>
     <h4>Data Sources</h4>
     <br>
     <p>A data source is a connection to an external rest API.<p>
@@ -9,58 +41,15 @@
         <li>The body accepts the standard json format</li>
       </ul>
     <p>
-      Template variables from the command ( {{ args.[index] }} ) are permitted in the data source URL and body,
+      <code>args</code> and <code>message</code> are permitted in the data source URL and body,
       but be careful not to create opportunities for injection attacks.
-    </p>
-    <h4> Templating </h4>
-    <br>
-    <p>
-      Templating can be used in data source fields and the final message template.
-    </p>
-    <p>
-       You can render a variable value in its place by reference using double braces:
-       <pre>{{ variable }}</pre>
-    </p>
-    <p>
-      If a variable has special characters, it can be rendered without escaping using:
-      <pre>{{{ variable }}}</pre>
-      However, be conscious that this poses an increased injection risk.
-    </p>
-
-    <h4> Available Data </h4>
-    <br>
-    <p>
-      All data uses dot notation, including arrays.
-    </p>
-    <p>
-      Bot command arguments are at:
-      <pre>{{ args.[index] }}</pre>
-      starting with the command itself at:
-      <pre>{{ args.0 }}</pre>
-      The <a href="https://core.telegram.org/bots/api#update" target="_blank">message</a> from the telegram update can be accessed at:
-      <pre>{{ message.[field]... }}</pre>
-      Data sources can be referenced by their assigned name.
-      <pre>{{ sources.[name] }}</pre><br>
-    </p>
-
-    <h4> Formatters </h4>
-    <br>
-    <p>
-      Formatters allow manipulation of the data.
-      <pre>{{ variable | formatter }}</pre>
-      They can be strung together in chains.
-      <pre>{ variable | formatter1 | formatter2 | ... }}</pre>
-      Currently only basic arithmetic functions are supported( add, subtract, multiply, divide)<br>
-      <pre>{{ args.1 | add: args.2 }}</pre>
-      will add two arguments together<br>
     </p>
 
     <h4> Additional Documentation </h4>
     <br>
     <p>
       Bot documentation:  <a href="https://core.telegram.org/bots/api" target="_blank">Telegram Bot API</a><br>
-      Templating documentation: <a href="https://github.com/janl/mustache.js" target="_blank">moustache.js Github</a><br>
-      Formatter documentation: <a href="http://jvitela.github.io/mustache-wax/" target="_blank">moustache-wax</a>
+      Templating documentation: <a href="https://ejs.co/" target="_blank">EJS</a>
     </p>
   </div>
 </template>
